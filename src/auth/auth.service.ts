5a3d80d215fcd388bb5e15a5f0963e6a702b0ced
Sahaparp Seekaewsiw <sahaparp.se@gmail.com>
2024-07-08 19:53:19 +0700

    fix auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -39,7 +39,6 @@ export class AuthService {
             });
 
             if(newUser && newUser.role === 'artist') {
-               console.log('newUser', newUser._id.toString());
                const newArtist = await this.artistRepository.save({
                     userId: newUser._id.toString(),
                });
@@ -52,53 +51,50 @@ export class AuthService {
             }
 
        } else  {
-          return response.status(422).send('Your error message or object here');
-          //   const status = response.status(422);
-          //   const msg = (response.statusMessage = 'This email has already been registered.');
-          //   return status;
-       }
-       const resultSave = await this.userRepository.save(user);
-       return { statusCode: 200, user: resultSave }
-    }
-
-    async login(user: User) {
-        const userVerify = await this.userRepository.findOne({
-            where: { email: user.email } 
-       });
-
-       if (!userVerify) {
-            return response.status(422).send('This email has not been registered.');
-       }
-
-       const passwordMatch = await bcrypt.compare(user.password, userVerify.password);
-       if (!passwordMatch) {
             const status = response.status(422);
-            const msg = (response.statusMessage = 'Password does not match.');
+            const msg = (response.statusMessage = 'This email has already been registered.');
             return status;
+       }
+    }
+
+//     async login(user: User) {
+//         const userVerify = await this.userRepository.findOne({
+//             where: { email: user.email } 
+//        });
+
+//        if (!userVerify) {
+//             return response.status(422).send('This email has not been registered.');
+//        }
+
+//        const passwordMatch = await bcrypt.compare(user.password, userVerify.password);
+//        if (!passwordMatch) {
+//             const status = response.status(422);
+//             const msg = (response.statusMessage = 'Password does not match.');
+//             return status;
             
-       }
+//        }
 
-       const userId = userVerify._id.toString();
-       const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
-       const userAuthenticationInfo = {
-            id: userVerify._id,
-            firstname: userVerify.firstname,
-            lastname: userVerify.lastname,
-            email: userVerify.email,
-            role: userVerify.role,
-            token: token,
-       }
+//        const userId = userVerify._id.toString();
+//        const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
+//        const userAuthenticationInfo = {
+//             id: userVerify._id,
+//             firstname: userVerify.firstname,
+//             lastname: userVerify.lastname,
+//             email: userVerify.email,
+//             role: userVerify.role,
+//             token: token,
+//        }
 
-       return { statusCode: 201, user: userAuthenticationInfo };
-    } 
+//        return { statusCode: 201, user: userAuthenticationInfo };
+//     } 
 
-    async getAllUser() {
-        return await this.userRepository.find();
-    }
+//     async getAllUser() {
+//         return await this.userRepository.find();
+//     }
 
-    async getUserById(id: string) {
-        const ObjectId = require('mongodb').ObjectId;
-        const user =  await this.userRepository.findOne(new ObjectId(id));
-        return user;
-    }
+//     async getUserById(id: string) {
+//         const ObjectId = require('mongodb').ObjectId;
+//         const user =  await this.userRepository.findOne(new ObjectId(id));
+//         return user;
+//     }
 }
